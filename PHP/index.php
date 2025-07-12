3d2c82b9b9dbaa2f7742fa8c0dccfb854c0f5398
yuukirintaynnyuuto <miki140518@gmail.com>
2025-07-12 11:36:06 +0900

    うし

---
+++ PHP/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tech-jam</title>
+</head>
+
+<!-- ゆうき -->
+<!-- http://localhost:8888/tech-jam/PHP/  -->
+
+
+
+<body>
+    <h1>Welcome to tech-jam</h1>
+    <p>こんにtは</p>
+    <p>ハロー</p>
+</body>
+
+</html>
