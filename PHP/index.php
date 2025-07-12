2a243a5172f76cd441fe65770f26f76dd6dfe93e
takenoko2008 <soyjoy12345joy@icloud.com>
2025-07-12 12:06:59 +0900

    コミット

--- PHP/index.php
+++ PHP/index.php
@@ -7,15 +7,14 @@
     <title>tech-jam</title>
 </head>
 
-<!-- ゆうき -->
 <!-- http://localhost:8888/tech-jam/PHP/  -->
-
 
 
 <body>
     <h1>Welcome to tech-jam</h1>
     <p>こんにtは</p>
     <p>ハロー</p>
+    <img src="/IMG/icon.jpg" alt="icon">
 </body>
 
 </html>
