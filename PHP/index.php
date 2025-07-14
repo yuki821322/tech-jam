abb9012b3eaf8b36c1aaf3fe81cd1ca93a1e2351
takenoko2008 <soyjoy12345joy@icloud.com>
2025-07-14 22:58:41 +0900

    モバイル対応

--- PHP/index.php
+++ PHP/index.php
@@ -2,21 +2,33 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tech-jam</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/tech-jam/CSS/index.css?v=2">
+  <title>tech-jam</title>
 </head>
 
+
 <!-- http://localhost:8888/tech-jam/PHP/  -->
-
-
 <body>
-    <h1>Welcome to tech-jam</h1>
-    <p>こんにtは</p>
-    <p>ハロー</p>
-    <img src="/IMG/icon.jpg" alt="icon">
-    <a href="login.php"><input type="button" value="ログイン"></a>
-    <a href="sign-up.php">ログインがお済みではない方は新規登録へ</a>
+<header>
+  <h2 class="title">Team-Task</h2>
+</header>
+
+<div class="main-layout">
+  <h1 class="main-title">Let’s<br>Create <br><span>Task.</span></h1>
+
+  <div class="container">
+    <b class="subtext">さぁTeam Taskを始めよう</b>
+    <p class="explanation">「Team Task」は、小規模なチームで<br>利用することに適しているチームタスク管理アプリです。</p>
+
+    <div class="login">
+      <a href="login.php"><input class="btn" type="button" value="ログイン"></a>
+      <a href="sign-up.php" class="sign-up">ログインがお済みではない方は新規登録へ</a>
+    </div>
+  </div>
+</div>
+
 </body>
 
 </html>
