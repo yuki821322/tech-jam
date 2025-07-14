8ae3a3b9a3609b771459e6887161271e33505270
yuukirintaynnyuuto <miki140518@gmail.com>
2025-07-14 11:39:11 +0900

    追記

--- PHP/complete.php
+++ PHP/complete.php
@@ -1,18 +1,27 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
 
-$registration_data = date('Y-m-d');
-
+$registration_date = date('Y-m-d');
 $file_name = '../CSV/user_data.csv';
 
-$fp = fopen($file_name, 'r');
-$cnt = 0;
-while (fgetcsv($fp) !== false) {
-    $cnt++;
-}
-fclose($fp);
+// 既存の最大IDを調べる
+$max_id = 0;
+if (file_exists($file_name)) {
+    $fp = fopen($file_name, 'r');
+    // ヘッダーがあればスキップ
+    $header = fgetcsv($fp);
 
-$id = $cnt;
+    while (($row = fgetcsv($fp)) !== false) {
+        if (isset($row[0]) && is_numeric($row[0])) {
+            $id = (int)$row[0];
+            if ($id > $max_id) {
+                $max_id = $id;
+            }
+        }
+    }
+    fclose($fp);
+}
+$new_id = $max_id + 1;
 
 $user_name = $_POST['username'];
 $email = $_POST['email'];
@@ -20,26 +29,29 @@ $birthday = $_POST['birthday'];
 $gender = $_POST['gender'];
 $password = $_POST['password'];
 
-
 $record = [
-    $id,
+    $new_id,
     $user_name,
     $email,
     $birthday,
     $gender,
     $password,
-    $registration_data
+    $registration_date
 ];
 
+// ファイルに追記
 $fp = fopen($file_name, 'a');
 if (flock($fp, LOCK_EX)) {
+    // ヘッダーがないファイルの場合はヘッダーを追加（ファイルサイズ0の場合）
+    if (filesize($file_name) === 0) {
+        fputcsv($fp, ['id', 'username', 'email', 'birthday', 'gender', 'password', 'registration_date']);
+    }
     fputcsv($fp, $record);
     flock($fp, LOCK_UN);
 } else {
     echo 'ファイルのロックに失敗しました。';
 }
 fclose($fp);
-
 ?>
 
 <!DOCTYPE html>
