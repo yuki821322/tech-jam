72f9231a338aaadfcd4253a9d00375a87bad0ede
yuukirintaynnyuuto <miki140518@gmail.com>
2025-07-15 16:26:50 +0900

    新規登録画面

--- PHP/index.php
+++ PHP/index.php
@@ -2,35 +2,35 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/tech-jam/CSS/index.css?v=2">
-    <title>tech-jam</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/tech-jam/CSS/index.css?v=2">
+  <title>tech-jam</title>
 </head>
 
 
 <!-- http://localhost:8888/tech-jam/PHP/  -->
 
 <body>
-    <header>
-        <h2 class="title">Team-Task</h2>
-    </header>
+  <header>
+    <h2 class="title">Team-Task</h2>
+  </header>
 
-    <div class="main-layout">
-        <h1 class="main-title">Let’s<br>Create <br><span>Task.</span></h1>
+  <div class="main-layout">
+    <h1 class="main-title">Let’s<br>Create <br><span>Task.</span></h1>
 
-        <div class="container">
-            <b class="subtext">さぁ<br>Team Taskを始めよう</b>
-            <p class="explanation">「Team Task」<br>小規模なチームで利用することに適しているチームタスク管理アプリです。</p>
+    <div class="container">
+      <b class="subtext">さぁ<br>Team Taskを始めよう</b>
+      <p class="explanation">「Team Task」<br>小規模なチームで利用することに適しているチームタスク管理アプリです。</p>
 
-            <div class="login">
-                <a href="login.php"><input class="btn" type="button" value="ログイン"></a>
-                <div class="sign-up-btn">
-                    <a href="sign-up.php" class="sign-up">新規登録</a>
-                </div>
-            </div>
+      <div class="login">
+        <a href="login.php"><input class="btn" type="button" value="ログイン"></a>
+        <div class="sign-up-btn">
+          <a href="sign-up.php" class="sign-up">新規登録</a>
         </div>
+      </div>
     </div>
+  </div>
 
 </body>
 
--- PHP/sign-up.php
+++ PHP/sign-up.php
@@ -4,60 +4,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/sign-up.css">
     <title>新規登録</title>
 </head>
 
 <body>
-    <h1>入力画面</h1>
-    <form action="confirmation.php" method="post">
-        <p>
-            <label for="username">名前:</label>
-            <input type="text" id="username" name="username" required>
-        </p>
-        <p>
-            <label for="email">メールアドレス:</label>
-            <input type="email" id="email" name="email" required>
-        </p>
-        <p>
-            <label for="birthday">生年月日:</label>
-            <input type="date" id="birthday" name="birthday" required>
-        </p>
-        <?php
-        // 性別の項目を管理する連想配列を作る
-        $gendar_list = [
-            1 => '男性',
-            2 => '女性',
-            3 => '未回答',
-        ];
-        ?>
-        <p>
-            性別<br>
-            <?php foreach ($gendar_list as $key => $value): ?>
-                <input type="radio" name="gender" id="gendar<?php echo $key; ?>"
-                    value="<?php echo $key; ?>"
-                    <?php if ($key ===  1)
-                        echo 'checked'
-                    ?>>
-                <label
-                    for="gendar<?php echo $key; ?>">
-                    <?php echo $value; ?>
-                </label>
-            <?php endforeach; ?>
-        </p>
-        <p>
-            <label for="password">パスワード:</label>
-            <input type="password" id="password" name="password" required>
-            <span>※パスワードは8文字以上で入力してください</span>
-        </p>
-        <p>
-            <label for="confirm_password">パスワード確認:</label>
-            <input type="password" id="confirm_password" name="confirm_password" required>
-            <span>※パスワードが一致することを確認してください</span>
-        </p>
-        <p>
-            <a href="confirmation.php"><input type="submit" value="確認画面へ"></a>
-        </p>
-    </form>
+    <section class="sign-all">
+        <div class="sign">
+            <h1>新規登録入力画面</h1>
+            <form action="confirmation.php" method="post">
+                <p>
+                    <input class="input" type="text" id="username" name="username" required placeholder="name">
+                </p>
+                <p>
+                    <input class="input" type="email" id="email" name="email" required placeholder="email">
+                </p>
+                <p>
+                    <input class="input" type="date" id="birthday" name="birthday" required placeholder="YYYY/MM/DD">
+                </p>
+                <?php
+                // 性別の項目を管理する連想配列を作る
+                $gendar_list = [
+                    1 => '男性',
+                    2 => '女性',
+                    3 => '未回答',
+                ];
+                ?>
+                <p>
+                    <?php foreach ($gendar_list as $key => $value): ?>
+                        <input type="radio" name="gender" id="gendar<?php echo $key; ?>"
+                            value="<?php echo $key; ?>"
+                            <?php if ($key ===  1)
+                                echo 'checked'
+                            ?>>
+                        <label
+                            for="gendar<?php echo $key; ?>">
+                            <?php echo $value; ?>
+                        </label>
+                    <?php endforeach; ?>
+                </p>
+                <p>
+                    <input class="input" type="password" id="password" name="password" required placeholder="Password">
+                    <br>
+                    <span>※パスワードは8文字以上で入力してください</span>
+                </p>
+                <p>
+                    <input class="input" type="password" id="confirm_password" name="confirm_password" required placeholder="Confirm">
+                    <br>
+                    <span>※パスワードが一致することを確認してください</span>
+                </p>
+                <p>
+                    <a href="confirmation.php"><input class="submit-button" type="submit" value="確認画面へ"></a>
+                </p>
+            </form>
+        </div>
+    </section>
 </body>
 
 </html>
