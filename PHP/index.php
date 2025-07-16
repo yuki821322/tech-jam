eb730195e596dc5073d301c014a34b3e6ca6429b
takenoko2008 <soyjoy12345joy@icloud.com>
2025-07-16 21:45:08 +0900

    スマホ画面修正

--- PHP/index.php
+++ PHP/index.php
@@ -4,12 +4,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/tech-jam/CSS/index.css?v=2">
+  <link rel="stylesheet" href="../CSS/index.css">
+
+
   <title>tech-jam</title>
 </head>
 
 
-<!-- http://localhost:8888/tech-jam/PHP/index.php  -->
+<!-- http://localhost:8888/tech-jam/php/index.php -->
 
 <body>
   <header>
@@ -18,7 +20,12 @@
   </header>
 
   <div class="main-layout">
-    <h1 class="main-title">Let’s<br>Create <br><span>Task.</span></h1>
+    <h1 class="main-title">
+      Let’s<br>
+      Create <br>
+      <span>Task.</span>
+    </h1>
+
 
     <div class="container">
       <b class="subtext">さぁ<br>Team Taskを始めよう</b>
