9299df3fb187325ba84d5bfc787b1d7788edc420
yuukirintaynnyuuto <miki140518@gmail.com>
2025-07-20 21:30:26 +0900

    from

--- PHPMAIN/from.php
+++ PHPMAIN/from.php
@@ -12,11 +12,24 @@ $csv_name = $_SESSION['csv_name'];
     <title>ようこそ</title>
 </head>
 
+<!-- http://localhost:8888/tech-jam/PHPMAIN/from.php -->
+
 <body>
+    <header>
+        <h1>tech-jam</h1>
+    </header>
     <h1>ようこそ、<?php echo htmlspecialchars($csv_name); ?> さん！</h1>
-    <div class="sidebar">サイドバー</div>
     <div class="all">
-        <div class="task">タスク管理画面</div>
+        <div class="sidebar">
+            <ul>
+                <li>カレンダー</li>
+                <li>会員詳細</li>
+                <li>お問合せ</li>
+            </ul>
+        </div>
+        <div class="task">
+            <h1>タスク管理</h1>
+        </div>
         <div class="progress">プログレスバー</div>
     </div>
 </body>
