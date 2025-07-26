b3c8f0e180bf41ae94ff2e8406ed6b2a4df1c863
yuukirintaynnyuuto <miki140518@gmail.com>
2025-07-26 18:27:29 +0900

    add

--- PHPMAIN/SIDEBAR/add-task.php
+++ PHPMAIN/SIDEBAR/add-task.php
@@ -12,7 +12,23 @@
     <form action="" method="">
         <p><input class="title" type="text" id="title" name="title" repuired placeholder="Title"></p>
         <p><input class="deadline" type="date" id="deadline" name="deadline" repuired placeholder="YYYY/MM/DD"></p>
+        <?php
+        // 性別の項目を管理する連想配列を作る
+        $format_list = [
+            1 => 'マルチ',
+            2 => 'シングル',
+        ];
+        ?>
+        <p>
+        <p>
+            <?php foreach ($format_list as $key => $value): ?>
+                <input type="radio" name="gender" id="fromat<?php echo $key; ?>"
+                    value="<?php echo $key; ?>" onclick="hyouji()">
+                <label for="gendar<?php echo $key; ?>"><?php echo $value; ?></label>
+            <?php endforeach; ?>
+        </p>
         <p><textarea class="form" name="content" value="" rows="3" cols="20" wrap="hard" repuired placeholder="Content"></textarea></p>
+        <p><input type="submit" value="追加"></p>
     </form>
 </body>
 
